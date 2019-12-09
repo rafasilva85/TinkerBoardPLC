b2f8a2d1608f1fc936e346f86005bb9286c5107e
rafasilva85 <rafa.tsystems@gmail.com>
2019-12-09 02:46:48 +0000

    Adding turnoff and correction to led.c

--- led.c
+++ led.c
@@ -2,14 +2,14 @@
 #include <wiringPi.h>
 
 // #define LED 0 matches with ASUS_GPIO 164! This can be checked with command 'sudo gpio readall'.
-#define RELAY0     31
-#define RELAY1     33
-#define RELAY2     35
-#define RELAY3     37
-#define RELAY4     32
-#define RELAY5     36
-#define RELAY6     38
-#define RELAY7     40
+#define RELAY0 0 
+#define RELAY1 2
+#define RELAY2 3
+#define RELAY3 21
+#define RELAY4 22
+#define RELAY5 23
+#define RELAY6 24
+#define RELAY7 25
 
 int main (void)
         {
@@ -23,42 +23,60 @@ int main (void)
         pinMode (RELAY6, OUTPUT);
         pinMode (RELAY7, OUTPUT);
 
+	digitalWrite(RELAY0,LOW);
+	digitalWrite(RELAY1,LOW);
+	digitalWrite(RELAY2,LOW);
+	digitalWrite(RELAY3,LOW);
+	digitalWrite(RELAY4,LOW);
+	digitalWrite(RELAY5,LOW);
+	digitalWrite(RELAY6,LOW);
+	digitalWrite(RELAY7,LOW);
+	
+	
         for (;;)
         {
-                digitalWrite (RELAY0, HIGH);
+		printf("LOOP_START\n");
+               	printf("CHANNEL_0\n");
+		digitalWrite (RELAY0, HIGH);
                 delay (500);
                 digitalWrite (RELAY0, LOW);
                 delay (500);
+		printf("CHANNEL_1\n");
                 digitalWrite (RELAY1, HIGH);
                 delay (500);
                 digitalWrite (RELAY1, LOW);
                 delay (500);
+		printf("CHANNEL_2\n");
                 digitalWrite (RELAY2, HIGH);
                 delay (500);
                 digitalWrite (RELAY2, LOW);
                 delay (500);
+		printf("CHANNEL_3\n");
                 digitalWrite (RELAY3, HIGH);
                 delay (500);
                 digitalWrite (RELAY3, LOW);
                 delay (500);
+		printf("CHANNEL_4\n");
                 digitalWrite (RELAY4, HIGH);
                 delay (500);
                 digitalWrite (RELAY4, LOW);
                 delay (500);
+		printf("CHANNEL_5\n");
                 digitalWrite (RELAY5, HIGH);
                 delay (500);
                 digitalWrite (RELAY5, LOW);
                 delay (500);
+		printf("CHANNEL_6\n");
                 digitalWrite (RELAY6, HIGH);
                 delay (500);
                 digitalWrite (RELAY6, LOW);
                 delay (500);
+		printf("CHANNEL_7\n");
                 digitalWrite (RELAY7, HIGH);
                 delay (500);
                 digitalWrite (RELAY7, LOW);
                 delay (500);
+		printf("LOOP_END\n");
          }
         return 0;
 }
-
-
